cc57287556352e2caf5aca661f1b45a35343e7cf
antonisg <antonis.garyfallou@scytalys.com>
2023-07-25 13:57:56 +0300

    Added Reports

--- src/api/PropertyApiService.jsx
+++ src/api/PropertyApiService.jsx
@@ -5,3 +5,5 @@ export const createPropertyApi = (property) => apiClient.post('/property', prope
 export const retrievePropertyApi = (tin) => apiClient.get(`property?tin=${tin}`);
 
 export const deletePropertyApi = (propertyId) => apiClient.delete(`/property/${propertyId}`);
+
+export const propertyRepairsReports = (userId, propertyId) => apiClient.get(`propertyRepair?userId=${userId}&propertyId=${propertyId}`);
