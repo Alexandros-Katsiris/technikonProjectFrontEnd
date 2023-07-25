c3ee2b7a9656441cb302fd3d31b9dd1f3dbcee39
konstantinosd <konstantinos.dimitriadis@scytalys.com>
2023-07-25 12:52:32 +0300

    Created horizontally displayed list of users

--- src/components/ListOfUsers.jsx
+++ src/components/ListOfUsers.jsx
@@ -59,23 +59,26 @@ const ListOfUsers = () => {
         </div>
       </div>
       <div style={{ columnCount: "6", marginTop: "40px", marginLeft: "80px" }}>
-        <h6>User Name</h6>
+        <h6>Name</h6>
+        <h6>Surname</h6>
         <h6>Phone Number</h6>
         <h6>E-mail</h6>
-        <h6>Date Created</h6>
-        <h6>Last Edited</h6>
-        <h6>Activities</h6>
+        <h6>Tax ID Number</h6>
+        <h6>Actions</h6>
       </div>
       <Container style={{ marginTop: "50px", maxWidth: "1000px" }}>
-        <ListGroup>
+        <ListGroup >
           {users.map((user) => {
             return (
-              <ListGroup.Item key={user.id}>
-                <Card style={{ width: "18rem" }}>
-                  <ListGroup variant="flush">
-                    <ListGroup.Item>{user.firstName}</ListGroup.Item>
-                    <ListGroup.Item>{user.surname}</ListGroup.Item>
-                    <ListGroup.Item>{user.phoneNumber}</ListGroup.Item>
+              <ListGroup.Item key={user.id} style={{marginLeft:'10px'}}>
+                <Card style={{ border: '0' }}>
+                  <ListGroup variant="" className = "list-group-horizontal-xxl w-100 p-3 " style={{ border: '0' }}  >
+                    <ListGroup.Item className="flex-fill">{user.firstName}</ListGroup.Item>
+                    <ListGroup.Item className="flex-fill">{user.surname}</ListGroup.Item>
+                    <ListGroup.Item className="flex-fill">{user.phoneNumber}</ListGroup.Item>
+                    <ListGroup.Item className="flex-fill">{user.email}</ListGroup.Item>
+                    <ListGroup.Item className="flex-fill">{user.tin}</ListGroup.Item>
+                    <ListGroup.Item className="flex-fill"><span className="badge bg-primary rounded-pill">update</span><span className="badge bg-primary rounded-pill">delete</span></ListGroup.Item>
                   </ListGroup>
                 </Card>
               </ListGroup.Item>
